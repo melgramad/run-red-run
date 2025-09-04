43946d743ca0ec41d93e570697e3fb04f0180649
hmbcs <hagerbahar.cs@gmail.com>
2025-09-04 01:28:42 -0400

    Structure main file to open a white window using pygame.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,29 @@
 ## current test file
 print("hello")
+
+import pygame
+
+pygame.init()
+
+## Set up a window
+
+screen = pygame.display.set_mode((500,700))
+pygame.display.set_caption("Pygame Test")
+
+## Background Fill to white
+screen.fill((255,255,255))
+pygame.display.flip()
+
+# Main loop to keep the window open and responsive
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+pygame.quit()
+
+print("Pygame is working!")
+
+
