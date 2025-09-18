24610f4e4c98524ae11519fe675766b4e72101a8
shahdd_06 <Sdrimaly@gmail.com>
2025-09-17 23:29:12 -0400

    Added Red riding hood character to screen

--- src/main.py
+++ src/main.py
@@ -1,32 +1,55 @@
-## current test file
-print("hello")
 
 import pygame
 
 pygame.init()
 
-## Set up a window
+# Set up a window
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
-screen = pygame.display.set_mode((500,700))
-pygame.display.set_caption("Pygame Test")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Run Red, Run!")
 
-## Background Fill to white
-screen.fill((255,255,255))
-pygame.display.flip()
+class Character(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load("orgsOfLangs/run-red-run/assets/standing.png")
+        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.rect = self.image.get_rect()#Controlling positions and collisions
+        self.rect.center = (x, y)
+
+    def draw(self):
+         screen.blit(self.image, self.rect)
+    
+
+
+player = Character(200,200, 1)
+
+        
+x = 200
+y = 200
+scale = 1
+
+
 
 # Main loop to keep the window open and responsive
-running = True
-while running:
+run = True
+while run:
+
+    player.draw()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            run = False
+
+    pygame.display.update()
+
 
 
 pygame.quit()
 
-print("Pygame is working!")
 
-print("is it working")
+
 
 
 
